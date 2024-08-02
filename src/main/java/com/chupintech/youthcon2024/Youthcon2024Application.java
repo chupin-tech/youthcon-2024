b517de19731646013070f0f986d4ef6de6f4d426
Sunghoon Park <monorisk@gmail.com>
2024-08-02 22:22:35 +0900

    init: Init project.
    1. Init project with the spring initializer.
    
    2. Add dependcies.
       - H2
       - Spring Data JPA
       - Junit

---
+++ src/main/java/com/chupintech/youthcon2024/Youthcon2024Application.java
@@ -0,0 +1,13 @@
+package com.chupintech.youthcon2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Youthcon2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Youthcon2024Application.class, args);
+	}
+
+}
